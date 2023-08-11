ba060634f7d2b6b0f35f85d5f1d4f6e71df05ae3
Sanjeev Sharma <ssharma2418@outlook.com>
2023-08-12 00:21:43 +0530

    python - developent

---
+++ Beginner-Level/itration.py
@@ -0,0 +1,13 @@
+
+numbers = [1, 2, 3, 4, 5, 6]
+
+for x in numbers:
+    if x == 3:
+        continue
+    print(x)
+    
+f = "fruits"
+for letter in f:
+    print(letter)
+    if letter == 'i':
+        break   
---
+++ Beginner-Level/range.py
@@ -0,0 +1,8 @@
+
+# for i in range(100):
+#    # print(i)
+# # This will print from 0 to 99
+for x in range(60,100):
+    print(x)
+    
+#this will print from  60 to 99
