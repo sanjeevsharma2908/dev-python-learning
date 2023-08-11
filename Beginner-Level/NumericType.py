16a892fc8b5b0999bc0b3f16206df3c98a087099
Sanjeev Sharma <ssharma2418@outlook.com>
2023-08-11 14:49:20 +0530

    Data-type

---
+++ Beginner-Level/NumericType.py
@@ -0,0 +1,17 @@
+
+a = 1 #int 
+b = 1.0 #float
+c = 1j # complex
+
+
+
+# convert int ->  float and vice versa
+x = float(a)
+y = int(b)
+
+
+
+print(type(x))
+print(type(y))
+z = complex(y)
+print(type(z))
---
+++ Beginner-Level/operation.py
@@ -0,0 +1,21 @@
+
+
+a = 18
+b = 20
+
+#addition 
+print(a+b)
+#subtraction
+print(b-a)
+#division 
+print(a/b)
+#multiplication
+print(a*b)
+#power or exponential
+print(a**b)
+#modulus
+print(a%b)
+#floor division
+print(a//b)
+
+# Floor division is the dividing one number by another and then rounding to the result to the closest integer that is smaller
