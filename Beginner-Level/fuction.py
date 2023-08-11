7620539108e6eb36a5e034c749ddf3bbd2b284e7
Sanjeev Sharma <ssharma2418@outlook.com>
2023-08-11 23:24:38 +0530

    function -in python

---
+++ Beginner-Level/fuction.py
@@ -0,0 +1,10 @@
+#function definition
+#parameter definitions
+def trialFunction(x,y):
+    print("Hello,",x,"Good Morning! Your Account Balance is :",y)
+    
+#function calling
+#arguments @ calling
+name = "Sanjeev Sharma"
+balance = 10000
+trialFunction(name,balance)    
