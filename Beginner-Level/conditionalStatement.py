d05af6f81a4846417d2a9fe66278f6886f6f83de
Sanjeev Sharma <ssharma2418@outlook.com>
2023-08-11 23:14:51 +0530

    conditional statement:

---
+++ Beginner-Level/conditionalStatement.py
@@ -0,0 +1,20 @@
+
+a = 3300
+b = 20000
+c = 500
+
+if a>b:
+    print('a is greater than b')
+elif a==b:
+    print('a is equals to  b')    
+else:
+    print('a is less than b')
+    
+    
+    
+if a>b and c>b:
+    print('a is greater than b and c')
+elif b>a and b<c:
+    print('b is greater than a and c')
+else:
+    print('c is greater than a and b')            
