2794607d18c9aff8ec3154d18fb2f87217e43115
Sanjeev Sharma <ssharma2418@outlook.com>
2023-08-12 15:58:42 +0530

    Fibonacci series

---
+++ checkIsEven.py
@@ -0,0 +1,13 @@
+
+
+def isEvenOrOdd(value):
+    if(value%2==0):
+        return True
+    
+x = 21
+
+
+if(isEvenOrOdd(x)):
+    print("Entered Value is Even" )
+else:
+    print("Entered Value is Odd" )       
